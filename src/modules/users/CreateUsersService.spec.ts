b0770ab3190afa0736e73c28ccc6ded5a484315f
Luiz Andre <luizxd234@gmail.com>
2020-07-06 20:34:54 -0300

    feat: model and repository interface of user

--- src/modules/users/CreateUsersService.spec.ts
+++
@@ -1,5 +0,0 @@
-describe('Testing jest', () => {
-  it('should be able to sum two numbers', async () => {
-    expect(2 + 1).toBe(3);
-  });
-});
---
+++ src/modules/users/infra/database/entities/User.ts
@@ -0,0 +1,40 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  username: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  admin: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
+
+export default User;
