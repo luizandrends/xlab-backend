c80b4fce74e2aa3a1df0e0d5751cc7d70d4989ed
Luiz Andre <luizxd234@gmail.com>
2020-07-06 19:39:50 -0300

    chore: first commit

---
+++ src/shared/infra/http/app.ts
@@ -0,0 +1,55 @@
+import 'reflect-metadata';
+
+import 'express-async-errors';
+
+import express, { Request, Response, NextFunction } from 'express';
+import path from 'path';
+
+import routes from '@shared/infra/http/routes';
+import AppError from '@shared/errors/AppError';
+
+class App {
+  public express: express.Application;
+
+  public constructor() {
+    this.express = express();
+
+    this.middlewares();
+    this.routes();
+    this.handleError();
+  }
+
+  private middlewares(): void {
+    this.express.use(express.json());
+    this.express.use(
+      '/users/files',
+      express.static(path.resolve(__dirname, '..', '..', '..', '..', 'tmp'))
+    );
+  }
+
+  private routes(): void {
+    this.express.use(routes);
+  }
+
+  private handleError(): void {
+    this.express.use(
+      (err: Error, request: Request, response: Response, _: NextFunction) => {
+        if (err instanceof AppError) {
+          return response.status(err.statusCode).json({
+            status: 'error',
+            message: err.message,
+          });
+        }
+
+        console.error(err);
+
+        return response.status(500).json({
+          status: 'error',
+          message: 'internal server error',
+        });
+      }
+    );
+  }
+}
+
+export default new App().express;
