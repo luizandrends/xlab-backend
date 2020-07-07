884a52eff9359e5a35c7cb5d621cbd7b35fe1766
Luiz Andre <luizxd234@gmail.com>
2020-07-06 21:28:00 -0300

    feat: registering the HashProvider singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,5 +1,7 @@
 import { container } from 'tsyringe';
 
+import '@modules/users/providers/index';
+
 import IUsersRepository from '@modules/users/interfaces/IUsersRepository';
 import UsersRepository from '@modules/users/infra/database/repositories/UsersRepository';
 
