e3e765758c2933f9929bb75d69fabf3f74c7ee1b
Luiz Andre <luizxd234@gmail.com>
2020-07-06 22:45:32 -0300

    fix: removing provider field from CreateUserController

--- src/modules/users/infra/http/controllers/CreateUserController.ts
+++ src/modules/users/infra/http/controllers/CreateUserController.ts
@@ -6,14 +6,13 @@ import CreateUserService from '@modules/users/services/CreateUserService';
 
 class UserController {
   public async create(request: Request, response: Response): Promise<Response> {
-    const { name, email, password, provider } = request.body;
+    const { name, email, password } = request.body;
 
     const createUser = container.resolve(CreateUserService);
     const user = await createUser.execute({
       name,
       email,
       password,
-      provider,
     });
 
     return response.json(classToClass(user));
