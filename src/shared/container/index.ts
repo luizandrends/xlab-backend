3f031f3da61c6d4ca7af70f2bae9ca0cda3e0f83
Luiz Andre <luizxd234@gmail.com>
2020-07-07 01:34:29 -0300

    feat: registering the singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -5,7 +5,15 @@ import '@modules/users/providers/index';
 import IUsersRepository from '@modules/users/interfaces/IUsersRepository';
 import UsersRepository from '@modules/users/infra/database/repositories/UsersRepository';
 
+import IDebtorsRepository from '@modules/debtors/interfaces/IDebtorsRepository';
+import DebtorsRepository from '@modules/debtors/infra/database/repositories/DebtorRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
+);
+
+container.registerSingleton<IDebtorsRepository>(
+  'DebtorsRepository',
+  DebtorsRepository
 );
