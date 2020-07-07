62e073bc7b749acbd1c4eb072efb6817ca50c64b
Luiz Andre <luizxd234@gmail.com>
2020-07-06 22:44:14 -0300

    feat: ShowUserService and tests

---
+++ src/modules/users/services/ShowuserService.spec.ts
@@ -0,0 +1,38 @@
+import AppError from '@shared/errors/AppError';
+
+import FakeUsersRepository from '../interfaces/fakes/FakeUsersRepository';
+import ShowProfileService from './ShowUserService';
+
+let fakeUsersRepository: FakeUsersRepository;
+let showProfile: ShowProfileService;
+
+describe('ShowUser', () => {
+  beforeEach(() => {
+    fakeUsersRepository = new FakeUsersRepository();
+
+    showProfile = new ShowProfileService(fakeUsersRepository);
+  });
+
+  it('should be able show the user', async () => {
+    const user = await fakeUsersRepository.create({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123456',
+    });
+
+    const profile = await showProfile.execute({
+      user_id: user.id,
+    });
+
+    expect(profile.name).toBe('John Doe');
+    expect(profile.email).toBe('johndoe@example.com');
+  });
+
+  it('should not be able show the user from non-existing user', async () => {
+    expect(
+      showProfile.execute({
+        user_id: 'non-existing-user-id',
+      })
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
