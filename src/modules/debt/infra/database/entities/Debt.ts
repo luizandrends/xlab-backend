a2998129f879bfe9a5ab107aafc875f2be5a6037
Luiz Andre <luizxd234@gmail.com>
2020-07-07 19:38:05 -0300

    chore: debt entitir

---
+++ src/modules/debt/infra/database/entities/Debt.ts
@@ -0,0 +1,37 @@
+import {
+  Entity,
+  Column,
+  PrimaryGeneratedColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+  DeleteDateColumn,
+} from 'typeorm';
+
+@Entity('debts')
+class Debt {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  user_id: string;
+
+  @Column()
+  debt_reason: string;
+
+  @Column()
+  date: Date;
+
+  @Column()
+  value: number;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+
+  @DeleteDateColumn()
+  deleted_at: Date;
+}
+
+export default Debt;
