694e6c59f5b570ce78b05173dc570e9b0ce8bbac
Luiz Andre <luizxd234@gmail.com>
2020-07-08 00:36:52 -0300

    chore: registering the singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -8,6 +8,9 @@ import UsersRepository from '@modules/users/infra/database/repositories/UsersRep
 import IDebtorsRepository from '@modules/debtors/interfaces/IDebtorsRepository';
 import DebtorsRepository from '@modules/debtors/infra/database/repositories/DebtorRepository';
 
+import IDebtRepository from '@modules/debt/interfaces/IDebtsRepository';
+import DebtsRepository from '@modules/debt/infra/database/repositories/DebtsRepository';
+
 container.registerSingleton<IUsersRepository>(
   'UsersRepository',
   UsersRepository
@@ -16,4 +19,9 @@ container.registerSingleton<IUsersRepository>(
 container.registerSingleton<IDebtorsRepository>(
   'DebtorsRepository',
   DebtorsRepository
+);
+
+container.registerSingleton<IDebtRepository>(
+  'DebtsRepository',
+  DebtsRepository
 );
