9014aecc8c123850b692c627282df617a4bb804b
Luiz Andre <luizxd234@gmail.com>
2020-07-08 00:34:41 -0300

    feat: DebtRepository

---
+++ src/modules/debt/infra/database/repositories/DebtRepository.ts
@@ -0,0 +1,69 @@
+import { Repository, getRepository } from 'typeorm';
+
+import Debt from '@modules/debt/infra/database/entities/Debt';
+
+import ICreateDebtDTO from '@modules/debt/dtos/ICreateDebtDTO';
+import IDebtsRepository from '@modules/debt/interfaces/IDebtsRepository';
+
+class FakeDebtsRepository implements IDebtsRepository {
+  private ormRepository: Repository<Debt>;
+
+  constructor() {
+    this.ormRepository = getRepository(Debt);
+  }
+
+  public async findById(debt_id: string): Promise<Debt | undefined> {
+    const findDebt = this.ormRepository.findOne({
+      where: { id: debt_id },
+    });
+
+    return findDebt;
+  }
+
+  public async findByDebtor(debtor_id: string): Promise<Debt | undefined> {
+    const findDebt = this.ormRepository.findOne({
+      where: { debtor_id },
+    });
+
+    return findDebt;
+  }
+
+  public async findByReason(
+    reason: string,
+    debtor_id: string
+  ): Promise<Debt | undefined> {
+    const findDebt = this.ormRepository.findOne({
+      where: { debt_reason: reason, debtor_id },
+    });
+
+    return findDebt;
+  }
+
+  public async create(data: ICreateDebtDTO): Promise<Debt> {
+    const debt = this.ormRepository.create(data);
+
+    await this.ormRepository.save(debt);
+
+    return debt;
+  }
+
+  public async list(): Promise<Debt[]> {
+    const listDebts = this.ormRepository.find();
+
+    return listDebts;
+  }
+
+  public async listByDebtor(debtor_id: string): Promise<Debt[]> {
+    const listDebts = this.ormRepository.find({
+      where: { debtor_id },
+    });
+
+    return listDebts;
+  }
+
+  public async save(debt: Debt): Promise<Debt> {
+    return this.ormRepository.save(debt);
+  }
+}
+
+export default FakeDebtsRepository;
